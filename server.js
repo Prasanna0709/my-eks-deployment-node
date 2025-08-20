09e916f312960cb5c077e16b08026457051ac594
prasanna0218 <prasannampmrasanna@gmail.com>
2025-08-20 16:08:36 +0530

    Buildspec file is added

--- server.js
+++ server.js
@@ -8,3 +8,13 @@ app.get("/hello",(req,res)=>{
 app.listen(3000,(req,res)=>{
     console.log("Server is running on port 3000");
 })
+
+/*
+
+subnet-04bf7fe19d695166c
+subnet-0e73e913fb8369fc1
+subnet-0c4e0d7572653a22e
+
+https://medium.com/@sanoj.sudo/how-to-create-aws-eks-cluster-step-by-step-a97420ede922
+
+*/
